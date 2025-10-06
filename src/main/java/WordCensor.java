11744514218a33a88af240d82ce3c57b375705e7
Khadija-1111 <sfkhadija.edu@gmail.com>
2025-10-06 19:28:46 -0400

    Update WordCensor.java

--- src/main/java/WordCensor.java
+++ src/main/java/WordCensor.java
@@ -4,7 +4,7 @@ public class WordCensor {
     public static void main(String[] args) {
         // TODO: Read a full sentence from the user using Scanner.nextLine()
         // TODO: Replace each banned word with exactly ***: dang, nuts, oops, yikes
-        // TODO: Print the result prefixed with: Censored:
+        // TODO: Print the result prefixed with: Censsored:
         // NOTE: Tests require exactly three asterisks (no ****) for every replacement
         Scanner scan = new Scanner(System.in);
         System.out.print("Input: ");
